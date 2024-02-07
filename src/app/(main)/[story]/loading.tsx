1673982880af1601cdd765ccbcc0f694f46b98a6
zerK4 <sebastian.v.pavel@gmail.com>
2024-02-07 20:17:53 +0200

    add loading state

---
+++ src/app/(main)/[story]/loading.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Loading() {
+  return <div className="bg-black/50 h-screen w-full animate-pulse" />;
+}
+
+export default Loading;
--- src/app/(main)/components/stories.tsx
+++ src/app/(main)/components/stories.tsx
@@ -18,29 +18,34 @@ function Stories({ stories }: { stories?: NavStories[] }) {
       {stories !== undefined &&
         stories.length !== 0 &&
         stories?.map(({ id, title, icon }) => (
-          <button
+          <Link
             key={id}
+            href={`/${id}`}
             className={`${pathName === `/${id}` && 'bg-accent'} group/story px-2 pl-10 pr-4 py-0.5 hover:bg-accent w-full text-left flex items-center justify-between ease-in-out duration-300 rounded-md`}
           >
-            <div className="flex items-center gap-2">
-              <IconPicker asChild onChange={(e) => handleEmojiUpdate(e, id)}>
-                <span className="hover:bg-primary h-8 w-8 rounded-sm flex items-center justify-center">
-                  {icon === null ? (
-                    <FileTextIcon size={14} />
-                  ) : (
-                    <span className="">{icon}</span>
-                  )}
-                </span>
-              </IconPicker>
-              <Link
-                href={`/${id}`}
-                className="whitespace-nowrap overflow-hidden max-w-[12rem]"
-              >
+            <button className="flex items-center justify-between w-full">
+              <div className="flex items-center gap-2">
+                <Link href={'#'}>
+                  <IconPicker
+                    asChild
+                    onChange={(e) => {
+                      handleEmojiUpdate(e, id);
+                    }}
+                  >
+                    <span className="hover:bg-primary h-8 w-8 rounded-sm flex items-center justify-center">
+                      {icon === null ? (
+                        <FileTextIcon size={14} />
+                      ) : (
+                        <span className="">{icon}</span>
+                      )}
+                    </span>
+                  </IconPicker>
+                </Link>
                 <span>{title}</span>
-              </Link>
-            </div>
-            <StoryDropMenu id={id} />
-          </button>
+              </div>
+              <StoryDropMenu id={id} />
+            </button>
+          </Link>
         ))}
     </div>
   );
